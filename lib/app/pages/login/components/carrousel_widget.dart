e70b4ca2fd103cdb4de11b6f611041fc1e1d2893
pedro henrique silva rodrigues <ph8464803@gmail.com>
2022-04-15 01:16:51 -0300

    fix: arrumando bug no carrousel de tela inicial
    Signed-off-by: pedro henrique silva rodrigues <ph8464803@gmail.com>

--- lib/app/pages/login/components/carrousel_widget.dart
+++ lib/app/pages/login/components/carrousel_widget.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class Carrousel extends StatelessWidget {
+class Carrousel extends StatefulWidget {
   Carrousel({Key? key}) : super(key: key);
-  List<String> images = [
+
+  @override
+  State<Carrousel> createState() => _CarrouselState();
+}
+
+class _CarrouselState extends State<Carrousel> {
+  List<String> imageList = [
     "https://www.getillustrations.com/packs/stories-vector-illustrations-for-websites/scenes/_1x/travel%20_%20%20travelling,%20airport,%20luggage,%20baggage,%20woman,%20female_md.png",
     "https://www.getillustrations.com/packs/flat-vector-illustrations-for-websites/scenes/_1x/travel%20_%20backpack,%20rucksack,%20navigation,%20location,%20pointer,%20left,%20right_md.png",
     "https://www.getillustrations.com/packs/humanic-character-illustrations-and-scenes-vector-pack/characters/_1x/travel,%20children,%20character%20builder%20_%20child,%20imagination,%20explore,%20discover_md.png",
@@ -12,13 +19,30 @@ class Carrousel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: PageView.builder(
-            itemCount: images.length,
-            pageSnapping: true,
-            itemBuilder: (context, pagePosition) {
-              return Container(
-                  margin: const EdgeInsets.only(left: 20, right: 20),
-                  child: Image.network(images[pagePosition]));
-            }));
+      margin: const EdgeInsets.only(top: 10),
+      alignment: Alignment.center,
+      child: CarouselSlider.builder(
+        itemCount: imageList.length,
+        options: CarouselOptions(
+          enlargeCenterPage: true,
+          height: 400,
+          autoPlay: true,
+          autoPlayInterval: const Duration(seconds: 3),
+          reverse: false,
+          aspectRatio: 5.0,
+        ),
+        itemBuilder: (context, i, id){
+          return GestureDetector(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image.network(
+                imageList[i],
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
